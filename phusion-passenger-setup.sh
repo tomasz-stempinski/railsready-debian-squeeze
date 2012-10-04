9e12f4e7ff0b3ea102a8ec844c36aecfe959285e
Tomasz Stempiński <tomasz.stempinski@gmail.com>
2012-10-05 00:44:08 +0200

    Added fix for gems cache folder.

--- phusion-passenger-setup.sh
+++ phusion-passenger-setup.sh
@@ -15,11 +15,12 @@ echo "==> done..."
 # Prepare sudo installation and show the env
 echo -e "\n=> Preparing apache-passenger installation..."
 rvmsudo bash -c export
-rvmsudo "rvm_path=/home/$script_runner/.rvm;passenger-install-apache2-module"
+rvmsudo "rvm_path=/home/$script_runner/.rvm;passenger-install-apache2-module --auto"
 echo "==> done..."
 
 # Install apache-passenger
 gem_dir=/home/$script_runner/.rvm/gems
+rm -rf "$gem_dir/cache"
 ruby_dir_full=`find $gem_dir/$ruby_dir* -maxdepth 0 -type d | head -1`
 passenger_dir=`find $ruby_dir_full/gems/passenger* -maxdepth 0 -type d | head -1`
 echo "Gem dir: $gem_dir"
@@ -31,3 +32,4 @@ sudo touch /etc/apache2/mods-available/passenger.load
 sudo su -c "echo 'LoadModule passenger_module $passenger_dir/ext/apache2/mod_passenger.so' >> /etc/apache2/mods-available/passenger.load"
 sudo touch /etc/apache2/mods-available/passenger.conf
 sudo su -c "echo 'PassengerRoot $passenger_dir' >> /etc/apache2/mods-available/passenger.conf"
+
